993c1f7cb65a25cd87164519edea4c35cd929982
Chris Matthieu <chris@matthieu.us>
2014-01-05 13:58:33 -0700

    added skynet to app

--- public/js/fly.js
+++ public/js/fly.js
@@ -9,101 +9,189 @@ var offset = 40
 var counter = 0
 var drone = { flying: false, y: 'steady', x: 'steady', z: 'steady' }
 
+var skynetConfig = {
+  "uuid": "1234567890",
+  "token": "1234567890"
+}    
+skynet(skynetConfig, function (e, socket) {
+// skynet(function (e, skynet) {
+// skynet({appName:'chris'}, function (e, skynet) {
+  if (e) throw e
 
-function doFrame(latestFrame){
-	var $log = $("#log")
-	$log.html("")
-	counter++
-	//$('#log').append(counter + '<br />')
+  console.log("Connected to Skynet!");
 
-	if(latestFrame.hands.length > 0){
-		if(!drone.flying) {
-			$.getJSON('/drone/takeoff', function(data) {
-				console.log('takeoff: ' + data.success)
-			})
-			drone.flying = true
-		}
-		var hand = latestFrame.hands[0]
+	function doFrame(latestFrame){
+		var $log = $("#log")
+		$log.html("")
+		counter++
+		//$('#log').append(counter + '<br />')
 
-		//console.clear()
-		//console.log(hand.direction)
+		if(latestFrame.hands.length > 0){
+			if(!drone.flying) {
+	
+	      socket.emit('message', {
+	        "devices": "*",
+	        "message": {
+	        	"fly":"takeoff"
+	      }}, function(data){
+					console.log('takeoff: ' + data)
+	      });         
 
-		// move up/down
-		if(hand.palmPosition[1] > base.position.y  + offset) {
-			$log.append('up' + '<br />')
-			if(drone.y != 'up') {
-				drone.y = 'up'
-				$.getJSON('/drone/y/up')
+				drone.flying = true
 			}
-		} else if(hand.palmPosition[1] < base.position.y  - offset) {
-			$log.append('down' + '<br />')
-			if(drone.y != 'down') {
-				drone.y = 'down'
-				$.getJSON('/drone/y/down')
-			}
-		}
-		else {
-			if(drone.y != 'steady') {
-				drone.y = 'steady'
-				$.getJSON('/drone/y/steady')
-			}
-		}
+			var hand = latestFrame.hands[0]
 
-		// move right/left
-		if(hand.palmPosition[0] > base.position.x  + offset) {
-			$log.append('right' + '<br />')
-			if(drone.x != 'right') {
-				drone.x = 'right'
-				$.getJSON('/drone/x/right')
+			//console.clear()
+			//console.log(hand.direction)
+
+			// move up/down
+			if(hand.palmPosition[1] > base.position.y  + offset) {
+				$log.append('up' + '<br />')
+				if(drone.y != 'up') {
+					drone.y = 'up'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"yaxis",
+		        	"y":"up"
+		      }}, function(data){
+						console.log('up: ' + data)
+		      });         
+
+				}
+			} else if(hand.palmPosition[1] < base.position.y  - offset) {
+				$log.append('down' + '<br />')
+				if(drone.y != 'down') {
+					drone.y = 'down'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"yaxis",
+		        	"y":"down"
+		      }}, function(data){
+						console.log('down: ' + data)
+		      });         
+				}
 			}
-		} else if(hand.palmPosition[0] < base.position.x  - offset) {
-			$log.append('left' + '<br />')
-			if(drone.x != 'left') {
-				drone.x = 'left'
-				$.getJSON('/drone/x/left')
+			else {
+				if(drone.y != 'steady') {
+					drone.y = 'steady'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"yaxis",
+		        	"y":"steady"
+		      }}, function(data){
+						console.log('steady: ' + data)
+		      });         
+				}
 			}
+
+			// move right/left
+			if(hand.palmPosition[0] > base.position.x  + offset) {
+				$log.append('right' + '<br />')
+				if(drone.x != 'right') {
+					drone.x = 'right'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"xaxis",
+		        	"x":"right"
+		      }}, function(data){
+						console.log('right: ' + data)
+		      });         
+				}
+			} else if(hand.palmPosition[0] < base.position.x  - offset) {
+				$log.append('left' + '<br />')
+				if(drone.x != 'left') {
+					drone.x = 'left'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"xaxis",
+		        	"x":"left"
+		      }}, function(data){
+						console.log('left: ' + data)
+		      });         
+				}
+			} else {
+				if(drone.x != 'steady') {
+					drone.x = 'steady'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"xaxis",
+		        	"x":"steady"
+		      }}, function(data){
+						console.log('steady: ' + data)
+		      });         
+				}
+			}
+
+			// move right/left
+			if(hand.palmPosition[2] > base.position.z  + offset) {
+				$log.append('backward' + '<br />')
+				if(drone.z != 'back') {
+					drone.z = 'back'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"zaxis",
+		        	"z":"back"
+		      }}, function(data){
+						console.log('back: ' + data)
+		      });         
+				}
+			} else if(hand.palmPosition[2] < base.position.z  - offset) {
+				$log.append('forward' + '<br />')
+				if(drone.z != 'front') {
+					drone.z = 'front'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"zaxis",
+		        	"z":"front"
+		      }}, function(data){
+						console.log('front: ' + data)
+		      });         
+				}
+			} else {
+				if(drone.z != 'steady') {
+					drone.z = 'steady'
+		      socket.emit('message', {
+		        "devices": "*",
+		        "message": {
+		        	"fly":"zaxis",
+		        	"z":"steady"
+		      }}, function(data){
+						console.log('steady: ' + data)
+		      });         
+				}
+			}
+			
+			if(latestFrame.gestures > 0) {
+				console.log(latestFrame.gestures)
+			}
+			
 		} else {
-			if(drone.x != 'steady') {
-				drone.x = 'steady'
-				$.getJSON('/drone/x/steady')
+			if(drone.flying) {
+
+	      socket.emit('message', {
+	        "devices": "*",
+	        "message": {
+	        	"fly":"land"
+	      }}, function(data){
+					console.log('land: ' + data)
+	      });         
+
+				drone.flying = false
 			}
+			$log.append('landing' + '<br />')
 		}
 
-		// move right/left
-		if(hand.palmPosition[2] > base.position.z  + offset) {
-			$log.append('backward' + '<br />')
-			if(drone.z != 'back') {
-				drone.z = 'back'
-				$.getJSON('/drone/z/back')
-			}
-		} else if(hand.palmPosition[2] < base.position.z  - offset) {
-			$log.append('forward' + '<br />')
-			if(drone.z != 'front') {
-				drone.z = 'front'
-				$.getJSON('/drone/z/front')
-			}
-		} else {
-			if(drone.z != 'steady') {
-				drone.z = 'steady'
-				$.getJSON('/drone/z/steady')
-			}
+		if($log.html() == '') {
+			$log.append('steady' + '<br />')
 		}
-		
-		if(latestFrame.gestures > 0) {
-			console.log(latestFrame.gestures)
-		}
-		
-	} else {
-		if(drone.flying) {
-			$.getJSON('/drone/land', function(data) {
-				console.log('land: ' + data.success)
-			})
-			drone.flying = false
-		}
-		$log.append('landing' + '<br />')
 	}
 
-	if($log.html() == '') {
-		$log.append('steady' + '<br />')
-	}
-}
+});	
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express')
   , path = require('path')
   , _ = require('underscore')
   , arDrone = require('ar-drone')
+  , skynet = require('skynet')
 
 var client  = arDrone.createClient()
 var publicDir = __dirname + '/public'
@@ -22,55 +23,64 @@ app.get('/', function(req, res) {
   res.sendfile(path.join(publicDir, 'index.html'))
 })
 
-app.get('/drone/takeoff', function(req, res){
-  client.takeoff()
-  res.json({success: true})
-})
-
-app.get('/drone/land', function(req, res){
-  client.stop()
-  client.land()
-  res.json({success: true})
-})
-
-app.get('/drone/y/:y', function(req, res){
-  if(req.params.y == 'up') {
-    client.up(0.4)
-  } 
-  else if(req.params.y == 'down') {
-    client.down(0.4)
-  } else {
-    client.up(0.0)
-  } 
-
-  res.json({success: true})
-})
-
-app.get('/drone/x/:x', function(req, res){
-  if(req.params.x == 'left') {
-    client.counterClockwise(0.6)
-  } 
-  else if(req.params.x == 'right') {
-    client.clockwise(0.6)
-  } else {
-    client.counterClockwise(0.0)
-  } 
-
-  res.json({success: true})
-})
-
-app.get('/drone/z/:z', function(req, res){
-  if(req.params.z == 'front') {
-    client.front(0.4)
-  } 
-  else if(req.params.z == 'back') {
-    client.back(0.4)
-  } else {
-    client.front(0.0)
-  } 
-
-  res.json({success: true})
-})
-
 var server = http.createServer(app)
 server.listen(process.env.PORT || 3000);
+
+var conn = skynet.createConnection({
+  "uuid": "0d3a53a0-2a0b-11e3-b09c-ff4de847b2cc",
+  "token": "qirqglm6yb1vpldixflopnux4phtcsor"
+});
+
+conn.on('ready', function(data){
+  console.log('Connected to Skynet');
+  conn.on('message', function(channel, databits){
+
+      console.log(databits);
+      data = JSON.parse(databits);
+
+      if(data.fly == 'takeoff'){
+        console.log("takeoff");
+        client.takeoff()
+
+      } else if(data.fly == 'land'){
+        console.log("land");
+        client.stop()
+        client.land()
+
+      } else if(data.fly == 'yaxis'){
+        console.log("yaxis");
+        if(data.y == 'up') {
+          client.up(0.4)
+        } 
+        else if(data.y == 'down') {
+          client.down(0.4)
+        } else {
+          client.up(0.0)
+        } 
+
+      } else if(data.fly == 'xaxis'){
+        console.log("xaxis");
+        if(data.x == 'left') {
+          client.counterClockwise(0.6)
+        } 
+        else if(data.x == 'right') {
+          client.clockwise(0.6)
+        } else {
+          client.counterClockwise(0.0)
+        } 
+
+      } else if(data.fly == 'zaxis'){
+        console.log("zaxis");
+        if(data.z == 'front') {
+          client.front(0.4)
+        } 
+        else if(data.z == 'back') {
+          client.back(0.4)
+        } else {
+          client.front(0.0)
+        } 
+      } 
+
+  });
+    
+});
